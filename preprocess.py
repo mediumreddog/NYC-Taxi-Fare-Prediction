d1d110970d09eca2b23674271d369390b6f0b038
Cliff Martin <cliff001002@gmail.com>
2018-11-06 20:45:19 -0600

    Fixed missing field on training data.

--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,7 @@ with open('data/train.csv') as training_data:
     with open('data/train_preprocessed.csv', 'a+') as training_data_out:
         data_writer = csv.writer(training_data_out)
         data_writer.writerow(['key', 'pickup_datetime', 'pickup_longitude', 'pickup_latitude',
-                              'dropoff_longitude', 'dropoff_latitude', 'passenger_count', 'distance', 'travel_time'])
+                              'dropoff_longitude', 'dropoff_latitude', 'passenger_count', 'distance', 'travel_time', 'fare_amount'])
 
         for row in data_reader:
             r = requests.get('http://127.0.0.1:5000/route/v1/driving/' +
@@ -19,8 +19,9 @@ with open('data/train.csv') as training_data:
 
             try:
                 data_writer.writerow([row['key'], row['pickup_datetime'], row['pickup_longitude'], row['pickup_latitude'], row['dropoff_longitude'],
-                                      row['dropoff_latitude'], row['passenger_count'], response['routes'][0]['distance'], response['routes'][0]['duration']])
+                                      row['dropoff_latitude'], row['passenger_count'], response['routes'][0]['distance'], response['routes'][0]['duration'], row['fare_amount']])
             except:
                 print "Error on Row Key: " + row['key']
 
-            print row['key']
+
+print 'Completed Successfully'
